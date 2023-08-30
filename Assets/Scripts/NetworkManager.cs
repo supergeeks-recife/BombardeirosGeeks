abdf9bed9d30b5c3915b947c0403de2d60cda885
kleberkgns <kleber.silva.kgns@gmail.com>
2023-08-30 16:24:58 -0300

    aula 03

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.UI;
+using TMPro;
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
+    public TMP_Text status;
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
     }
 
+    void Update()
+    {
+        status.text = "Servidor: " + PhotonNetwork.CloudRegion + "\nPing: " + PhotonNetwork.GetPing();
+    }
+
     public override void OnConnected()
     {
-        Debug.Log("Conexão concluída com sucesso");
+        Debug.Log("ConexÃ£o concluida com sucesso");
     }
 
     public override void OnConnectedToMaster()
     {
         Debug.Log("O mestre chegou!");
         Debug.Log("Servidor: " + PhotonNetwork.CloudRegion + " Ping: " + PhotonNetwork.GetPing());
+        
     }
 
 }
